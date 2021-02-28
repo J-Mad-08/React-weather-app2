fd83f912494441ae4b02883d435c0d70afe0b390
J-Mad-08 <65929815+J-Mad-08@users.noreply.github.com>
2021-02-27 23:10:08 -0500

    updated efficiency and reduced states
    Added bootstrap and axios, updated react scripts, styling updates, reduced states and made an object to hold my axios calls

--- src/Row.js
+++ src/Row.js
@@ -2,99 +2,57 @@ import React, { useState } from "react";
 import axios from "axios";
 
 import "./Row.css";
-import Date from "./Date";
 
 export default function Row() {
   // State
-  const [showInput, handleInput] = useState(null);
-  const [result, showResult] = useState(false);
-  const [weather, displayWeather] = useState(null);
+  const [forecast, setForecast] = useState({ ready: false });
 
-  function changeData(event) {
-    handleInput(event.target.value);
-  }
+  function handleResponse(response) {
+    //console.log("other ", response.data);
 
-  //call from API
-  function showTemp(response) {
-    console.log("response is ", response.data.main.name);
-    //console.log(response.data.weather);
-    displayWeather({
-      temperature: Math.round(response.data.main.temp),
-      description: Math.round(response.data.weather[0].description),
-      temp_max: Math.floor(response.data.main.temp_max),
-      temp_min: Math.floor(response.data.main.temp_min),
-      wind: Math.floor(response.data.wind.speed),
-      humidity: Math.floor(response.data.main.humidity),
+    //instead of making multiple states for the responses, place all into an obj
+    setForecast({
+      ready: true,
+      minTemp: response.data.main.temp_min,
+      temperature: response.data.main.temp,
+      humidity: response.data.main.humidity,
+      date: new Date(response.data.dt * 1000),
+      description: response.data.weather[0].description,
+      icon: response.data.weather[0].icon,
+      wind: response.data.wind.speed,
+      city: response.data.name,
+      humanFeel: response.data.main.feels_like,
     });
+    // console.log("forecast is ", forecast);
   }
 
-  function searching(event) {
-    event.preventDefault();
-    // Axios
-    const apiKey = "5d58512296f20bed286330764deb9e8d";
-    //or 80995efae6a984b21a5fdccf75f0b0c2
-    const units = "metric";
-
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${showInput}&apiKey=${apiKey}&units=${units}`;
-    //api.openweathermap.org/data/2.5/weather?q=Brooklyn&apiKey=5d58512296f20bed286330764deb9e8d&units=metric
-    //console.log(apiUrl);
-    axios.get(apiUrl).then(showTemp);
-
-    showResult(weather);
-  }
-
-  //storing form
-  const form = (
-    <div>
-      <div className="form">
-        <section className="user-input">
-          <nav className="navbar">
-            <form className="Searching" onSubmit={searching}>
-              <input
-                type="search"
-                className="form-control mr-sm-2"
-                placeholder="Search a city"
-                onChange={changeData}
-              ></input>
-              <input
-                type="submit"
-                value="Search"
-                className="btn btn-outline-dark"
-                id="dark-button"
-                autoFocus="on"
-              ></input>
-              <button
-                type="button"
-                className="btn btn-outline-dark"
-                id="current-button"
-              >
-                Current
-              </button>
-            </form>
-          </nav>
-        </section>
-      </div>
-    </div>
-  );
-
-  //conditional render
-  if (result) {
+  //conditional
+  if (forecast.ready) {
     return (
       <div>
-        {/* <Date /> */}
-        {/* fix the below */}
-        <h2>
-          {showInput.slice(0, 1).toUpperCase().concat(showInput.slice(1))}
-        </h2>
+        <h2>{forecast.city}</h2>
         <div className="row">
           <section className="current">
             <div className="col">
-              <legend className="current-temp" id="curTemp"></legend>
-              <span id="emoji"> ☀️</span>
-              {/* NEED A COMPONENT TO GRAB CURRENT TEMP */}
-              <span id="current-temp"> {weather.temp_max} </span>
-              {form}
-
+              <legend className="current-temp"></legend>
+              <span id="curTemp"> {forecast.icon}</span>
+              <span id="curTemp"> {Math.round(forecast.temperature)} </span>
+              <a href="#" id="converter">
+                <small id="celsius">ºC</small>|<small id="fahrenheit">ºF</small>
+              </a>
+              <div>
+                {/* date should go here */}
+                <div className="dateForecast">{Math.round(forecast.date)}</div>
+                {/* slogan */}
+                <p className="slogan">
+                  {" "}
+                  {forecast.description
+                    .slice(0, 1)
+                    .toUpperCase()
+                    .concat(forecast.description.slice(1))}
+                </p>
+              </div>
+              {/* additional weather goes here, high, humidity, wind... */}
               <div className="col">
                 <div className="todays-additional-weather">
                   <br />
@@ -103,28 +61,66 @@ export default function Row() {
                     {" "}
                     <bold>Low Temp:</bold>{" "}
                   </em>
-                  <strong id="temp-max">{weather.temp_max} %</strong>
-                  <strong id="temp-min">{weather.temp_min} % </strong>
+                  <strong id="temp-max">
+                    {Math.round(forecast.temperature)} %
+                  </strong>
+                  <strong id="temp-min">
+                    {Math.round(forecast.minTemp)} %{" "}
+                  </strong>
                   <br />
-                  <strong id="wind">Wind: {weather.wind}k/h</strong>
+                  <strong id="wind">
+                    Wind: {Math.round(forecast.wind)}k/h
+                  </strong>
                   <br />
-                  <strong id="humidity">Humidity: {weather.humidity}%</strong>
+                  <strong id="humidity">Humidity: {forecast.humidity}%</strong>
                   <p className="slogan"></p>
                 </div>
               </div>
             </div>
           </section>
         </div>
+        {/* form */}
+        <div className="form">
+          <section className="user-input">
+            <nav className="navbar">
+              <form className="Searching">
+                {/* onSubmit={searching} */}
+                <input
+                  type="search"
+                  className="form-control mr-sm-2"
+                  placeholder="Search a city"
+                  // onChange={changeData}
+                ></input>
+                <input
+                  type="submit"
+                  value="Search"
+                  className="btn btn-outline-dark"
+                  id="dark-button"
+                  autoFocus="on"
+                ></input>
+                <button
+                  type="button"
+                  className="btn btn-outline-dark"
+                  id="current-button"
+                >
+                  Current
+                </button>
+              </form>
+            </nav>
+          </section>
+        </div>
       </div>
     );
+  } else {
+    // Axios
+    const apiKey = "5d58512296f20bed286330764deb9e8d";
+    //or 80995efae6a984b21a5fdccf75f0b0c2
+    const units = "metric";
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Marietta&apiKey=${apiKey}&units=${units}`;
+    //api.openweathermap.org/data/2.5/weather?q=Brooklyn&apiKey=5d58512296f20bed286330764deb9e8d&units=metric
+    //console.log(apiUrl);
+    axios.get(apiUrl).then(handleResponse);
+    //storing form
+    return null;
   }
-  //form
-  return (
-    <div>
-      <div className="city">Brooklyn</div>
-      <Date />
-      {form}
-      {/* {showTemp("Brooklyn")} */}
-    </div>
-  );
 }
